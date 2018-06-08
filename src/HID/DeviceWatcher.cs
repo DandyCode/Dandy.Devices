5d41adf40cffcf764527be114fb35cd55bb4c9ed
David Lechner <david@lechnology.com>
2018-06-08 12:57:12 -0500

    remove unused code

--- src/HID/DeviceWatcher.cs
+++ src/HID/DeviceWatcher.cs
@@ -1,36 +1,10 @@
 using System;
-using System.Reactive;
-using System.Reactive.Disposables;
-using System.Reactive.Linq;
-using System.Reactive.Subjects;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
 namespace Dandy.Devices.HID
 {
-    public interface IPlatformDeviceWatcher : IDisposable
-    {
-        void Start();
-
-        void Stop();
-
-        event EventHandler<DeviceEventArgs> Added;
-        event EventHandler<DeviceEventArgs> Changed;
-        event EventHandler<DeviceEventArgs> Removed;
-        event EventHandler<EventArgs> Stopped;
-    }
-
-    public sealed class DeviceEventArgs : EventArgs
-    {
-        public string Id { get; }
-
-        public DeviceEventArgs(string id)
-        {
-            Id = id ?? throw new ArgumentNullException(nameof(id));
-        }
-    }
-
-    public sealed class DeviceWatcher : ObservableBase<Device>, IConnectableObservable<Device>
+    public static class DeviceWatcher
     {
         /// <summary>
         /// Gets an HID device watcher for the current platform.
@@ -50,40 +24,5 @@ namespace Dandy.Devices.HID
 
             throw new NotSupportedException("The current platform is not supported");
         }
-
-        readonly IPlatformDeviceWatcher watcher;
-        readonly IObservable<Device> addedObservable;
-        readonly IObservable<DeviceEventArgs> changedObservable;
-        readonly IObservable<DeviceEventArgs> removedObservable;
-        readonly IObservable<EventArgs> stoppedObservable;
-
-        public DeviceWatcher(IPlatformDeviceWatcher watcher)
-        {
-            this.watcher = watcher ?? throw new ArgumentNullException(nameof(watcher));
-            removedObservable = Observable.FromEvent<EventHandler<DeviceEventArgs>, DeviceEventArgs>(
-                x => watcher.Added += x, x => watcher.Added -= x
-            );
-        }
-
-        public IDisposable Connect()
-        {
-            watcher.Start();
-            return Disposable.Create(() => watcher.Stop());
-        }
-
-        protected override IDisposable SubscribeCore(IObserver<Device> observer)
-        {
-            var notifier = observer.ToNotifier();
-
-            watcher.Added += (s, e) => {
-                var device = new Device(e.Id);
-                notifier(Notification.CreateOnNext(device));
-                removedObservable.Where(x => x.Id == e.Id);
-            };
-
-            return null;
-        }
-
-        public void Dispose() => watcher.Dispose();
     }
 }
